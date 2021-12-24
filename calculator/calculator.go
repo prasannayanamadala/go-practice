66649a83557a99bfcc795d4fbc588a615f0b96f7
prasannaraavi <50182956+prasannayanamadala@users.noreply.github.com>
2021-12-23 21:59:01 -0500

    Add files via upload

---
+++ calculator/calculator.go
@@ -0,0 +1,12 @@
+package calculator
+
+import "fmt"
+
+func Add(x int, y int) {
+
+	fmt.Println(x + y)
+}
+func Sub(x int, y int) {
+
+	fmt.Println(x - y)
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "go-practice/calculator"
+
+func main() {
+	calculator.Add(1, 2)
+	calculator.Sub(1, 2)
+}
